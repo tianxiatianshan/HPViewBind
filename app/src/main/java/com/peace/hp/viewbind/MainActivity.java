b85ab74990c76169e1b04ffb87ce176580f08cfd
何平 <heping114551@pwrd.com>
2019-10-30 16:48:04 +0800

    初始化提交

---
+++ app/src/main/java/com/peace/hp/viewbind/MainActivity.java
@@ -0,0 +1,14 @@
+package com.peace.hp.viewbind;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
